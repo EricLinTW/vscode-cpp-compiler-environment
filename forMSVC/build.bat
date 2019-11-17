4f96e4d58508f2878deed856d8074f48d334e2eb
EricLin <eric888a@hotmail.com.tw>
2019-11-18 03:19:02 +0800

    configer MSVC version

--- forMSVC/build.bat
+++ forMSVC/build.bat
@@ -1,7 +1,6 @@
 @echo off
 title test build command
-set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
-set QTDIR=C:\Qt\Qt5.9.1\5.9.1\msvc2015_64
-set PATH=%PATH%;%QTDIR%\bin
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
+set PATH=%PATH%;
 call "%VCINSTALLDIR%\vcvarsall.bat" amd64
 nmake
